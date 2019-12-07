1da7f3a80b42ef2804b3817260cf452acdd55312
Erol Yurtkuran <erol@yurtkuran.net>
2019-12-07 13:46:06 -0500

    added express-validator form validation

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -5,6 +5,35 @@ const mongoose = require('mongoose');
 // load employee schema
 const Employee = require('../models/Employee.model');
 
+// express validator middleware
+const { check, validationResult } = require('express-validator');
+
+// validation options
+const employeeValidation = [
+    check('fullName').not().isEmpty().withMessage('This is a required field.'),
+    check('email').isEmail().withMessage('Invalid e-mail address'),
+    check('city').not().isEmpty().withMessage('This is a required field.'),
+    check('email').custom((email, { req }) => {
+
+        // check if email exists in database
+        return Employee.find({ email }).then(employee => {
+            
+            // email exists
+            if (employee.length > 0) {
+
+                // determine new or existing employee
+                if (req.body._id == '') {                               // new employee
+                    return Promise.reject('E-mail already in use');
+                } else if (req.body._id != employee[0]._id) {           // existing employee
+                    return Promise.reject('E-mail already in use');
+                }
+            
+            }
+
+        });
+    }),
+]
+
 // display employee form
 router.get('/', (req, res) => {
     res.render('employee/addOrEdit', {
@@ -13,11 +42,30 @@ router.get('/', (req, res) => {
 });
 
 // add employee to database
-router.post('/', (req,res) => {
-    if (req.body._id == '')
-        insertRecord(req,res);
-    else
-        updateRecord(req, res);
+router.post('/', [
+
+    //form validation
+    employeeValidation
+
+    ], (req,res) => {
+
+    // process validation errors, if any
+    const errors = validationResult(req).errors;
+
+    if (errors === undefined || errors.length == 0) {
+        // no errors, add to database and render list
+        if (req.body._id == '')
+            insertRecord(req, res);
+        else
+            updateRecord(req, res);
+    } else {
+        handleValidationError(errors, req.body);
+        res.render("employee/addOrEdit", {
+            viewTitle: "Insert Employee",
+            employee: req.body
+        });
+    }
+
 });
 
 // list employees
@@ -81,14 +129,8 @@ const insertRecord = (req, res) => {
         if (!err) {
             res.redirect('employee/list')
         } else {
-
-            // check for validation error
             if (err.name == 'ValidationError') {
-                handleValidationError(err, req.body);
-                res.render("employee/addOrEdit", {
-                    viewTitle: "Insert Employee",
-                    employee: req.body
-                });
+                console.log('Validation error occured during database insert.');
             } else {
                 console.log('Error during record insertion: ' + err);
             }
@@ -104,13 +146,8 @@ const updateRecord = (req, res) => {
         if (!err) {
             res.redirect('/employee/list');
         } else {
-            // check for validation error
             if (err.name == 'ValidationError') {
-                handleValidationError(err, req.body);
-                res.render("employee/addOrEdit", {
-                    viewTitle: "Update Employee",
-                    employee: req.body
-                });
+                console.log('Validation error occured during database update.');
             } else {
                 console.log('Error during record update: ' + err);
             }
@@ -119,19 +156,12 @@ const updateRecord = (req, res) => {
 }
 
 // process validation errors, store to body
-const handleValidationError = (err, body) => {
-    for (var field in err.errors) {
-        switch (err.errors[field].path) {
-            case 'fullName':
-                body['fullNameError'] = err.errors[field].message;
-                break;
-            case 'email':
-                body['emailError'] = err.errors[field].message;
-                break;
-            default:
-                break;
-        }
-    }
+const handleValidationError = (errors, body) => {
+
+    errors.forEach(error => {
+        body[error.param+'Error'] = error.msg;
+    });
+
 }
 
 module.exports = router;
--- models/Employee.model.js
+++ models/Employee.model.js
@@ -21,10 +21,4 @@ const employeeSchema = new mongoose.Schema({
   }
 });
 
-// Custom validation for email
-employeeSchema.path('email').validate((val) => {
-    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(val);
-}, 'Invalid e-mail.');
-
 module.exports = Employee = mongoose.model('Employee', employeeSchema);
